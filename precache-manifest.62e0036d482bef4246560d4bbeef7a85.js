7aace74a5f03d35980bd188b80b769115f4481f7
MarcosCaamal <m04caamal@gmail.com>
2020-06-25 16:26:58 -0500

    Updates

---
+++ precache-manifest.62e0036d482bef4246560d4bbeef7a85.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdcadb6f22d2a87de9a0d61954bc54fb",
+    "url": "/twittor-app/index.html"
+  },
+  {
+    "revision": "d0df7f490c89b3a458ee",
+    "url": "/twittor-app/static/css/2.9f98d992.chunk.css"
+  },
+  {
+    "revision": "b4f646bfe487fe5c857b",
+    "url": "/twittor-app/static/css/main.66880981.chunk.css"
+  },
+  {
+    "revision": "d0df7f490c89b3a458ee",
+    "url": "/twittor-app/static/js/2.12d7b43e.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/twittor-app/static/js/2.12d7b43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f646bfe487fe5c857b",
+    "url": "/twittor-app/static/js/main.30fcdc23.chunk.js"
+  },
+  {
+    "revision": "69a3e65e33a313e41554",
+    "url": "/twittor-app/static/js/runtime-main.c424c03d.js"
+  },
+  {
+    "revision": "9b45fe492d3b935b1a283ece2e1b5ac8",
+    "url": "/twittor-app/static/media/calendar.9b45fe49.svg"
+  },
+  {
+    "revision": "16e35662baa4ca9e4f4344fd51fb7ec6",
+    "url": "/twittor-app/static/media/camara.16e35662.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/twittor-app/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "d3c57e3b34eea06328b31dbb81bcabc4",
+    "url": "/twittor-app/static/media/error404.d3c57e3b.png"
+  },
+  {
+    "revision": "a108520eea84cd603eeaf8eeda7676b1",
+    "url": "/twittor-app/static/media/link.a108520e.svg"
+  },
+  {
+    "revision": "41f65f75cb0b41db6fe44ab4b074bacc",
+    "url": "/twittor-app/static/media/logo-white.41f65f75.png"
+  },
+  {
+    "revision": "1769587d7e1330e9d4241c30f672c7ff",
+    "url": "/twittor-app/static/media/logo.1769587d.png"
+  },
+  {
+    "revision": "02f2070b4161235fe7d8909276457b53",
+    "url": "/twittor-app/static/media/ubication.02f2070b.svg"
+  }
+]);
